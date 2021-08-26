814191d5eee210370a7753a1555ac9e00258faec
Aritro Shome <aritroshome.development@gmail.com>
2021-08-26 13:24:13 +0530

    Skeleton

---
+++ template.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define lli long long int
+#define vi vector<int>
+#define nl "\n"
+#define pb push_back
+
+
+
+void start()
+{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);	
+}
+
+void solve()
+{
+	
+}
+
+int main()
+{
+	start();
+	int T;
+	cin >> T;
+	while(T--)
+	{
+		solve();
+	}
+	return 0;
+}
